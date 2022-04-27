45a24eda9c3198de2e345cabe643478499bf7844
Naveen Kamatham <naveen_kamatham@thbs.india.com>
2022-04-27 12:26:22 +0530

    27/04/2022

--- SpringBoot/src/main/java/com/example/demo/Controller.java
+++ SpringBoot/src/main/java/com/example/demo/Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Control {
+public class Controller {
 	
 	
 	@RequestMapping("/")
